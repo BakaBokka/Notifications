dc41a4cf5d8a3e44b538cd0bd875b09dbac40839
Damir Vakhitov <damirvakhitov@gmail.com>
2021-03-26 22:00:01 +0300

    docs: Добавил описание и ссылку на деплой в readme

--- src/components/SetReadButton/SetReadButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./SetReadButton.scss";
-
-function SetReadButton() {
-  return  <button className="SetReadButton">Прочитать</button>
-
-}
-
-export default SetReadButton;
